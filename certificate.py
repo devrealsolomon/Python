f516160201d61755581eccbfc14a682df3cac5e1
Solomon Olatunji <realsolomon07@gmail.com>
2022-12-16 05:21:24 +0100

    update

--- certificate.py
+++ certificate.py
@@ -1,28 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont
 import requests
 from io import BytesIO
-# Create a blank image with a white background
 width, height = 400, 300
 image = Image.new('RGB', (width, height), (255, 255, 255))
 
-# Draw the certificate title on the image
 draw = ImageDraw.Draw(image)
 title_text = 'Certificate of Completion'
 title_font = ImageFont.truetype('arial.ttf', 36)
 title_width, title_height = draw.textsize(title_text, font=title_font)
 draw.text(((width - title_width) / 2, 50), title_text, fill=(0, 0, 0), font=title_font)
 
-# Draw the certificate content on the image
 content_text = 'This certificate is awarded to:'
 content_font = ImageFont.truetype('arial.ttf', 18)
 content_width, content_height = draw.textsize(content_text, font=content_font)
 draw.text(((width - content_width) / 2, 100), content_text, fill=(0, 0, 0), font=content_font)
 
-# Download an image of a USA signature
 response = requests.get('https://www.example.com/signature.png')
 signature_image = Image.open(BytesIO(response.content))
 
-# Paste the signature image onto the certificate image
 image.paste(signature_image, (100, 200))
 
 # Save the certificate image as a JPEG file
--- id.py
+++ id.py
@@ -2,37 +2,30 @@ from PIL import Image, ImageDraw, ImageFont
 import requests
 from io import BytesIO
 
-# Create a blank image with a white background
 width, height = 400, 300
 image = Image.new('RGB', (width, height), (255, 255, 255))
 
-# Download a photo of the ID card holder
 response = requests.get('https://www.noblenaija.com/omo.jpg')
 photo_image = Image.open(BytesIO(response.content))
 
-# Paste the photo image onto the ID card image
 image.paste(photo_image, (50, 50))
 
-# Draw the ID card holder's name on the image
 draw = ImageDraw.Draw(image)
 name_text = 'John Smith'
 name_font = ImageFont.truetype('arial.ttf', 36)
 name_width, name_height = draw.textsize(name_text, font=name_font)
 draw.text((200, 50), name_text, fill=(0, 0, 0), font=name_font)
 
-# Draw the company name on the image
 company_text = 'Acme Corporation'
 company_font = ImageFont.truetype('arial.ttf', 18)
 company_width, company_height = draw.textsize(company_text, font=company_font)
 draw.text((200, 100), company_text, fill=(0, 0, 0), font=company_font)
 
-# Draw the ID number on the image
 id_text = 'ID: 123456'
 id_font = ImageFont.truetype('arial.ttf', 18)
 id_width, id_height = draw.textsize(id_text, font=id_font)
 draw.text((200, 150), id_text, fill=(0, 0, 0), font=id_font)
 
-# Draw the issued date on the image
 date_text = 'Issued: January 1, 2020'
 date_font = ImageFont.truetype('arial.ttf', 18)
 date_width, date_height = draw.textsize(date_text, font=date_font)
